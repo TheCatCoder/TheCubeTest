84e9c8fd31eb004357317195f644cff49bff5c21
spandan22 <spandan.musser@gmail.com>
2015-11-23 19:23:43 -0700

    I added some Jquery to make the footer stick to the bottom of the other pages that aren't long enough to force it down; I also revised the button code to make it maintain a fixed width that will suit the  space required for text inside it

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
-            $("#hider").hide();
+    $("#hider").hide();
 
-            $("#button").hover(
-                function () {
-                    $("#hider").fadeIn(1000).show();
-                    $("#firstVisible").fadeOut(500).hide();
-                },
-                function () {
-                    $("#firstVisible").fadeIn(500).show();
-                    $("#hider").hide();
-                }
-            );
-        });
+    $("#button").hover(
+        function () {
+            $("#hider").fadeIn(500).show();
+            $("#firstVisible").fadeOut(500).hide();
+        },
+        function () {
+            $("#firstVisible").fadeIn(500).show();
+            $("#hider").hide();
+        }
+    );
+    var bodyHeight = $("body").height();
+    var vwptHeight = $(window).height();
+    if (vwptHeight > bodyHeight) {
+        $("footer#stickyFoot").css("position", "absolute").css("bottom", 0);
+    }
+});
